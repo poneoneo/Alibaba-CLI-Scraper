7ff1f4f3a8b234c1ad83698f95aa68cd3f907fe7
poneoneo <onealzero@gmail.com>
2024-05-07 16:33:04 -0400

    refactor(scrapper): delete scrapper.py file

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Integer, String,Boolean
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+
 @dataclass
 class Product(Base):
     __tablename__ = 'person'
