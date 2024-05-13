a9b09cb20db13afcf12ccfae9c0a80c1b27730ee
poneoneo <onealzero@gmail.com>
2024-05-13 14:49:09 -0400

    create two tables Products and Suppliers and commit any changes to sqlite database

--- models.py
+++ models.py
@@ -1,12 +1,27 @@
-from dataclasses import dataclass
-from sqlalchemy import Column, Integer, String,Boolean
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from pony.orm import Database, Required, PrimaryKey,Set,Optional
+
+db = Database()
+
+class Product(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    alibaba_guranteed = Required(bool)
+    certifications = Required(str)
+    minimum_to_order = Required(int)
+    ordered_or_sold = Required(int)
+    supplier = Required('Supplier')
+    min_price = Required(float)
+    max_price = Required(float)
+
+class Supplier(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    products = Set('Product')
+    name = Required(str)
+    verification_mode = Required(str)
+    sopi_level = Required(int)
+    country_name = Optional(str)
+    years_as_gold_supplier = Required(int)
 
 
-@dataclass
-class Product(Base):
-    __tablename__ = 'person'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    age = Column(Integer)
+db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
+db.generate_mapping(create_tables=True)
