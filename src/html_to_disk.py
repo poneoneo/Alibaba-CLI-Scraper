9c48b46be6e2707b9cc83446d4a118dd4407f359
poneoneo <onealzero@gmail.com>
2024-05-19 19:27:14 -0400

    refactor(project): track project from src/ folder

---
+++ src/html_to_disk.py
@@ -0,0 +1,22 @@
+
+
+from pathlib import Path
+from loguru import logger
+
+
+def _create_folder(folder_name:str):
+    logger.info(f"Create folder with {folder_name} as name if not exists yet ...")
+    Path(f"{folder_name}").mkdir(exist_ok=True)
+    return Path(f"{folder_name}")
+
+@logger.catch()
+def write_to_disk(folder_name:str, pages_contents: list[str]):
+    logger.info("Saving your scrapping result in disk...")
+    path_obj = _create_folder(folder_name)
+    for page_number,content in enumerate(pages_contents):
+            with open((path_obj/f"page_{page_number+1}.html").resolve() ,'a+',encoding='utf-8') as file:
+                    file.write(content)
+            logger.info(f"Html content from page {page_number+1} has been saved succesfully !")
+            
+
+if __name__ =="__name__":...
