aab032b437998a715b4ffdc2293b5dc81fe834f0
poneoneo <onealzero@gmail.com>
2024-06-11 00:39:30 -0400

    fix: fix many errors with response body from page and add sync api

--- src/html_to_disk.py
+++ src/html_to_disk.py
@@ -2,12 +2,37 @@
 
 from pathlib import Path
 from loguru import logger
+import selectolax
 
 
 def _create_folder(folder_name:str):
     logger.info(f"Create folder with {folder_name} as name if not exists yet ...")
     Path(f"{folder_name}").mkdir(exist_ok=True)
     return Path(f"{folder_name}")
+
+def _browser_parser(html_content: str|bytes,):
+    """Parse the HTML content of the page to get the divs with class `.organic-list.viewtype-list`
+
+    :param html_content: The HTML content of the page
+    :type html_content: str
+    :return: A list of divs with class `.organic-list.viewtype-list`
+    :rtype: list
+    """
+    if html_content is None:
+        logger.warning(" your page could not be loaded, an empty none value has been returned")
+        return None
+    body_parser = selectolax.parser.HTMLParser(html_content)
+    product_div = body_parser.css_first(".organic-list")
+    if product_div is None:
+        return None
+    html_result = product_div.html
+    if html_result is None:
+        logger.warning(
+            "any HTML content  match the selector '.organic-list', None value has been returned"
+            )
+        return None
+    else: 
+        return html_result
 
 @logger.catch()
 def write_to_disk(folder_name:str, pages_contents: list[str]):
@@ -36,8 +61,11 @@ def write_to_disk(folder_name:str, pages_contents: list[str]):
     logger.info("Saving your scrapping result in disk...")
     path_obj = _create_folder(folder_name)
     for page_number,content in enumerate(pages_contents):
+            parsed_content = _browser_parser(html_content=content)
+            if parsed_content is None:
+                continue
             with open((path_obj/f"page_{page_number+1}.html").resolve() ,'a+',encoding='utf-8') as file:
-                    file.write(content)
+                    file.write(parsed_content)
             logger.info(f"Html content from page {page_number+1} has been saved succesfully !")
             
 
