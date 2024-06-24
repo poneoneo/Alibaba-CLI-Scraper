2ce71f993a5915cec4b0ba59bfbb69e3abbb4013
poneoneo <onealzero@gmail.com>
2024-06-23 23:09:23 -0400

    feat(*): shutdown debug logger and add sync-api as a new feature. finally increase async speed

--- src/app.py
+++ src/app.py
@@ -1,174 +1,223 @@
+import asyncio
 import json
+import os
+import sys
 from pathlib import Path
 from typing import Optional
-from click import MissingParameter
+
+import sqlalchemy
 import typer
-import sys
-from web_scrapper import async_scrapper
-import asyncio
+from click import MissingParameter
+from dotenv import load_dotenv
+from engine_and_database import (
+    add_products_to_db,
+    add_suppliers_to_db,
+    create_db_engine,
+    save_all_changes,
+)
+from info_message import update_db_success_sqlite, update_db_with_success
 from loguru import logger
+from rich import print as rprint
 from scrape_from_disk import PageParser
-from engine_and_database import add_products_to_db, create_db_engine,save_all_changes,add_suppliers_to_db
 from sqlmodel import SQLModel
 from typing_extensions import Annotated
-from rich import print as rprint
+from web_scrapper import async_scrapper, sync_scrapper
+
+load_dotenv()
 
 logger.remove(0)
-logger.add(sys.stderr, colorize=True)
+logger.add(sys.stderr, colorize=True, level=os.environ.get("LOGURU_LEVEL", ""))
 
-app = typer.Typer()
+app = typer.Typer(pretty_exceptions_enable=False)
 
-def _db_url(credentials:dict=dict(),auto_fill:bool=False):
+
+def _db_url(credentials: dict = dict(), auto_fill: bool = False):
     if auto_fill is True:
         if Path("db_credentials.json").exists() is False:
-            raise FileNotFoundError("db_credentials.json is missing YOU NEED TO RUN `db-init` COMMAND FIRST !!")
-        with open("db_credentials.json","r") as f:
+            raise FileNotFoundError(
+                "db_credentials.json is missing YOU NEED TO RUN `db-init` COMMAND FIRST !!"
+            )
+        with open("db_credentials.json", "r") as f:
             cred = json.load(f)
         return f"mysql+mysqldb://{cred.get('user')}:{cred.get('password')}@{cred.get('host')}/{cred.get('db_name')}"
     else:
         cred = {
-            "user":credentials.get("user"),
-            "password":credentials.get("password"),
-            "host":credentials.get("host"),
-            "port":credentials.get("port"),
-            "db_name":credentials.get("db_name"),
-        }   
-        for key,value in cred.items():
+            "user": credentials.get("user"),
+            "password": credentials.get("password"),
+            "host": credentials.get("host"),
+            "port": credentials.get("port"),
+            "db_name": credentials.get("db_name"),
+        }
+        for key, value in cred.items():
             if value is None:
                 raise MissingParameter(f"{key} is missing")
-        with open("db_credentials.json","w") as f:
-            json.dump(cred,f)
-        return f"mysql+mysqldb://{cred.get('user')}:{cred.get('password')}@{cred.get('host')}/{cred.get('db_name')}"  
+        with open("db_credentials.json", "w") as f:
+            json.dump(cred, f)
+        return f"mysql+mysqldb://{cred.get('user')}:{cred.get('password')}@{cred.get('host')}/{cred.get('db_name')}"
+
 
 @app.command()
 def run_scrapper(
-        key_words:Annotated[str,typer.Argument(help="Keywords to search")],
-        html_folder:Annotated[Optional[str],typer.Option(help="Folder to save the results")]=None,
-        ) -> None:
+    key_words: Annotated[str, typer.Argument(help="Keywords to search")],
+    html_folder: Annotated[
+        Optional[str], typer.Option(help="Folder to save the results")
+    ] = None,
+    sync_api: Annotated[
+        Optional[bool], typer.Option(help="Folder to save the results")
+    ] = False,
+) -> None:
     """
-    Runs the web scrapper to search for keywords and save the results in a specified folder.
-    
+    Runs the web scrapper and loking for products and related suppliers infos based on provided keywords, then save the results in a specified folder by default this folder is named with the keywords that was provided and could attached with '_' if there are spaces in the keywords'.
+
     Args:
         key_words (str): The keywords to search for.
         html_folder (Optional[str], optional): The folder to save the results. Defaults to None.
-    
+
     Returns:
         None
-    
+
     Raises:
         None
     """
-    
-    save_in_folder = key_words.strip().replace(" ","_") if (html_folder is None and html_folder != '') else html_folder
-    asyncio.run(async_scrapper(save_in=save_in_folder,key_words=key_words))
-    rprint("[bold white]Step :one:  is [bold green] done  :star: ! [/bold green] [/bold white]  ")
-    rprint("[bold white blink]Scrapper has been completed [bold green]succesfully :white_heavy_check_mark-emoji: ![/bold green] [/bold white blink]")
-    rprint("[bold white]Here we go to the step :two:   [/bold white]  ")
-    rprint(f"[bold white blink]Now all pages results matching your keywords has been saved in [magenta]{save_in_folder}[/magenta] directory you can now append all products and suppliers into your database with [bold magenta]`db-update --kw-results {save_in_folder}`[/bold magenta] command[/bold white blink]")
+
+    save_in_folder = (
+        key_words.strip().replace(" ", "_")
+        if (html_folder is None and html_folder != "")
+        else html_folder
+    )
+    if sync_api:
+        sync_scrapper(save_in=save_in_folder, key_words=key_words)
+    else:
+        asyncio.run(async_scrapper(save_in=save_in_folder, key_words=key_words))
+
 
 @app.command()
 def db_update(
-    db_engine:Annotated[Optional[str],typer.Argument(help="Name of database engine to use")]='sqlite',
-    kw_results=typer.Option(default=...,help="Folder where the html results are stored"),
-    filename:Annotated[Optional[str],typer.Option(help="Name of the sqlite file(without any extensions) to update with news data",)]=None,
+    db_engine: Annotated[
+        Optional[str], typer.Argument(help="Name of database engine to use")
+    ] = "sqlite",
+    kw_results=typer.Option(
+        default=..., help="Folder where the html results are stored"
+    ),
+    filename: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Name of the sqlite file(without any extensions) to update with news data",
+        ),
+    ] = None,
 ):
     """
     Updates the database with new products and their related suppliers.
-
-    Args:
-        db_engine (str, optional): The name of the database engine to use. Defaults to 'sqlite'.
-        kw_results (str): The folder where the HTML results are stored.
-        filename (str, optional): The name of the sqlite file (without any extensions) to update with news data. Defaults to None.
-
     Raises:
         MissingParameter: If the engine is not 'sqlite' or 'mysql', or if the filename is required for sqlite engine, or if the filename is specified for non-sqlite engine.
 
     Returns:
         None
     """
-    if db_engine not in ["sqlite","mysql"]:
+    if db_engine not in ["sqlite", "mysql"]:
         raise MissingParameter("--engine should be sqlite or mysql")
     if db_engine == "sqlite" and filename is None:
-        raise MissingParameter("--filename is required for sqlite engine" )
-    if db_engine !="sqlite" and filename is not None:
-        raise MissingParameter("You don't need to specify --filename for non-sqlite engine" )
+        raise MissingParameter("--filename is required for sqlite engine")
+    if db_engine != "sqlite" and filename is not None:
+        raise MissingParameter(
+            "You don't need to specify --filename for non-sqlite engine"
+        )
     if db_engine == "mysql":
         db_url = _db_url(auto_fill=True)
         mysql_engine = create_db_engine(db_url=db_url)
         page_parser = PageParser(targeted_folder=kw_results)
         suppliers = page_parser.detected_suppliers()
         products = page_parser.detected_products()
-        add_suppliers_to_db(suppliers=suppliers,engine_db=mysql_engine)
-        add_products_to_db(products=products,engine_db=mysql_engine)
-        save_all_changes(engine_db=mysql_engine,sql_model=SQLModel)
-        rprint("[bold white]Step :three:  is [bold green] done  :star: ! [/bold green] [/bold white]")
-        rprint("[bold white blink]Your database has been updated with new products and their related suppliers :white_heavy_check_mark-emoji: ![/bold white blink]")
-        rprint("[bold white blink]Now you can do some amazing analysis woth your database for making your business more profitable :star: :star: ! [/bold white blink]")
-     
+        add_suppliers_to_db(suppliers=suppliers, engine_db=mysql_engine)
+        add_products_to_db(products=products, engine_db=mysql_engine)
+        save_all_changes(engine_db=mysql_engine, sql_model=SQLModel)
+        update_db_with_success()
+
     else:
         page_parser = PageParser(targeted_folder=kw_results)
         suppliers = page_parser.detected_suppliers()
         products = page_parser.detected_products()
-        sqlite_engine = create_db_engine(db_name=filename) # type: ignore
-        add_suppliers_to_db(suppliers=suppliers,engine_db=sqlite_engine)
-        add_products_to_db(products=products,engine_db=sqlite_engine)
-        save_all_changes(engine_db=sqlite_engine,sql_model=SQLModel)
-        rprint(f"[bold white] [magenta bold] {filename}.sqlite [/magenta bold] file has been updated succesfully  with new products and their related suppliers :white_heavy_check_mark-emoji: ![/bold white]")
-        rprint("[bold white]Step :three:  is [bold green] done  :star: ! [/bold green] [/bold white]  ")
-        rprint("[bold white blink]Now you can do some amazing analysis with your database for making your business more profitable :star: :star: ! [/bold white blink]")
+        sqlite_engine = create_db_engine(db_name=filename)  # type: ignore
+        add_suppliers_to_db(suppliers=suppliers, engine_db=sqlite_engine)
+        add_products_to_db(products=products, engine_db=sqlite_engine)
+        save_all_changes(engine_db=sqlite_engine, sql_model=SQLModel)
+        update_db_success_sqlite(sqlite_file=kw_results)
 
 
 @app.command()
 def db_init(
-        engine:Annotated[str,typer.Argument(help="Name of database engine to use")]='sqlite',
-        sqlite_file:Annotated[Optional[str],typer.Option(help="Name of the sqlite file to use (without any extensions)",show_default=False)]=None,
-        host:Annotated[Optional[str],typer.Option(help="Host of the database engine",)]="localhost",
-        port:Annotated[Optional[int],typer.Option(help="Port of the database engine")]=3306,
-        user:Annotated[Optional[str],typer.Option(help="User of the database engine",show_default=False)]=None,
-        password:Annotated[Optional[str],typer.Option(help="Password of the database engine",show_default=False)]=None,
-        db_name:Annotated[Optional[str],typer.Option(help="Database of the database engine",show_default=False)]=None,
-  ):
+    engine: Annotated[
+        str, typer.Argument(help="Name of database engine to use")
+    ] = "sqlite",
+    sqlite_file: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Name of the sqlite file to use (without any extensions)",
+            show_default=False,
+        ),
+    ] = None,
+    host: Annotated[
+        Optional[str],
+        typer.Option(
+            help="Host of the database engine",
+        ),
+    ] = "localhost",
+    port: Annotated[
+        Optional[int], typer.Option(help="Port of the database engine")
+    ] = 3306,
+    user: Annotated[
+        Optional[str],
+        typer.Option(help="User of the database engine", show_default=False),
+    ] = None,
+    password: Annotated[
+        Optional[str],
+        typer.Option(help="Password of the database engine", show_default=False),
+    ] = None,
+    db_name: Annotated[
+        Optional[str],
+        typer.Option(help="Database of the database engine", show_default=False),
+    ] = None,
+):
     """
     Initializes a database using the specified engine.
-
-    Args:
-        engine (str): The name of the database engine to use. Defaults to 'sqlite'.
-        sqlite_file (Optional[str]): The name of the sqlite file to use (without any extensions).
-            Required for 'sqlite' engine.
-        host (Optional[str]): The host of the database engine. Defaults to 'localhost'.
-        port (Optional[int]): The port of the database engine.
-        user (Optional[str]): The user of the database engine.
-        password (Optional[str]): The password of the database engine.
-        db_name (Optional[str]): The database of the database engine.
 
     Raises:
         MissingParameter: If the engine is not 'sqlite' or 'mysql', or if the filename is required for sqlite engine,
             or if the filename is specified for non-sqlite engine.
 
-    Returns:
-        None
 
     This function initializes a database using the specified engine. It checks the validity of the engine and the
     required parameters. If the engine is 'sqlite', it creates a sqlite database file with the specified name. If the
     engine is 'mysql', it creates a mysql database using the specified host, port, user, password, and database name.
-    After creating the database, it saves the changes to the database using the `save_all_changes` function. Finally,
+    After creating the database, it saves the changes to the database. Finally,
     it prints a success message indicating the creation of the database.
     """
-    if engine not in ["sqlite","mysql"]:
+    if engine not in ["sqlite", "mysql"]:
         raise MissingParameter("--engine should be sqlite or mysql")
     if engine == "sqlite" and sqlite_file is None:
-        raise MissingParameter("--sqlite-file is required for sqlite engine" )
-    if engine !="sqlite" and sqlite_file is not None:
-        raise MissingParameter("You dont need to specify --sqlite-file for non-sqlite engine")
+        raise MissingParameter("--sqlite-file is required for sqlite engine")
+    if engine != "sqlite" and sqlite_file is not None:
+        raise MissingParameter(
+            "You dont need to specify --sqlite-file for non-sqlite engine"
+        )
     if engine == "mysql":
         db_url = _db_url(credentials=locals())
         mysql_engine = create_db_engine(db_url=db_url)
-        save_all_changes(engine_db=mysql_engine,sql_model=SQLModel)
-        rprint(f"[bold white]Database [magenta bold] {db_name} [/magenta bold] has been created succesfully :white_heavy_check_mark-emoji: ![/bold white]")
+        try:
+            save_all_changes(engine_db=mysql_engine, sql_model=SQLModel)
+            rprint(
+                f"[bold white]Database [magenta bold] {db_name} [/magenta bold] has been created succesfully :white_heavy_check_mark-emoji: ![/bold white]"
+            )
+        except sqlalchemy.exc.OperationalError as e:
+            if "Unknown database" in str(e):
+                rprint(
+                    f"[bold white blink] It's seems like you want to use mysql engine but you haven't created database: [magenta bold] `{db_name}` [/magenta bold] ![/bold white blink] create it first and then try again"
+                )
+            logger.error(f"Errors has occured: {e}")
     else:
-        sqlite_engine = create_db_engine(db_name=sqlite_file) # type: ignore
-        save_all_changes(engine_db=sqlite_engine,sql_model=SQLModel)
-        rprint(f"[bold white]Database [magenta bold] {sqlite_file}.sqlite [/magenta bold] file has been created succesfully :white_heavy_check_mark-emoji: ![bold white]")
-              
+        sqlite_engine = create_db_engine(db_name=sqlite_file)  # type: ignore
+        save_all_changes(engine_db=sqlite_engine, sql_model=SQLModel)
+
+
 if __name__ == "__main__":
     app()
--- src/web_scrapper.py
+++ src/web_scrapper.py
@@ -9,29 +9,30 @@ managing environment variables.
 """
 
 import asyncio
-
 import time
 
-
-from loguru import logger
-from playwright.async_api import async_playwright,BrowserContext,Browser
-from playwright.async_api import Page as AsyncPage
-from playwright.sync_api import sync_playwright
-from dotenv import load_dotenv
-import os
+import playwright
 import selectolax
-
+from dotenv import load_dotenv
 from html_to_disk import write_to_disk
+from info_message import run_scrapper_with_success
+from loguru import logger
+from playwright.async_api import Page as AsyncPage
+from playwright.async_api import async_playwright
+from playwright.sync_api import sync_playwright
+from rich import print as rprint
+from rich.progress import Progress, TaskID, SpinnerColumn, TimeElapsedColumn
+from rich.console import Console
+import os
 
 load_dotenv()
 
 
-SBR_WS_CDP_LIST: str = "wss://brd-customer-hl_82bed799-zone-scraping_browser1:scn06xx7tquc@brd.superproxy.io:9222"  # type: ignore
+SBR_WS_CDP_LIST: str = os.environ["SBR_WS_CDP_LIST"]
 
 html_content_list = []
 
-
-def _browser_parser(html_content: str|bytes, curr_url:str):
+def _browser_parser(html_content: str | bytes, curr_url: str):
     """Parse the HTML content of the page to get the divs with class `.organic-list.viewtype-list`
 
     :param html_content: The HTML content of the page
@@ -40,12 +41,14 @@ def _browser_parser(html_content: str|bytes, curr_url:str):
     :rtype: list
     """
     if html_content is None:
-        logger.warning(" your page could not be loaded, an empty none value has been returned")
+        logger.warning(
+            " your page could not be loaded, an empty none value has been returned "
+        )
         return None
     body_parser = selectolax.parser.HTMLParser(html_content)
     logger.info(
-                f"looking for .organic-list class in the DOM page {curr_url.split('page=')[1]}..."
-            )
+        f"looking for .organic-list class in the DOM page {curr_url.split('page=')[1]}... "
+    )
     product_div = body_parser.css_first(".organic-list")
     if product_div is None:
         return None
@@ -53,16 +56,22 @@ def _browser_parser(html_content: str|bytes, curr_url:str):
     if html_result is None:
         logger.warning(
             f"any HTML content from page {curr_url.split('page=')[1]} matched the selector '.organic-list', None value has been returned"
-            )
+        )
         return None
-    else: 
-        logger.info(f"HTML content from page {curr_url.split('page=')[1]} has been returned ")
+    else:
+        logger.info(
+            f"HTML content from page {curr_url.split('page=')[1]} has been returned  "
+        )
         return html_result
+
 
 async def goto_task(
     url: str,
-    page: AsyncPage
-) -> str|None:
+    page: AsyncPage,
+    task: TaskID,
+    progress: Progress,
+    semaphore: asyncio.Semaphore,
+) -> str | None:
     """Return the entire HTML content from each page only the divs with class `.organic-list.viewtype-list`
 
     :param url: URL of page range 1 to 42 include
@@ -74,26 +83,26 @@ async def goto_task(
     """
     try:
         async with page:
-            logger.info(f"Loading page {url.split('page=')[1]} ...")
-            await page.wait_for_load_state('domcontentloaded')
-            response = await page.goto(url, wait_until='domcontentloaded',timeout=0)    
+            logger.info(f"Loading page {url.split('page=')[1]} ... ")
+            response = await page.goto(url, wait_until="domcontentloaded", timeout=0)
             if response is None:
                 return None
-            logger.info(f"Returns html representation of response body from page {url.split('page=')[1]} ...")
+            _lock = asyncio.Lock()
+            # async with _lock:
+            #     async with semaphore:
+            logger.info(f"get response text from web page {url.split('page=')[1]} ... ")
             html_body = await response.text()
-            # parser_result = await asyncio.to_thread(_browser_parser, html_content=html_body, curr_url=url)
-            logger.info(f"Closing page {url.split('page=')[1]} ...")
+            progress.start_task(task)
+            progress.update(task, advance=2.385)
+            logger.info(f"Closing page {url.split('page=')[1]} ... ")
             await page.close()
-            return html_body
-            if html_body is None:
-                await page.close()
-                return None
-            await page.close()
+            # logger.info(
+            #     f"Returns html representation of response body from page {url.split('page=')[1]} ... "
+            # )
             return html_body
     except Exception as e:
         logger.error(f"Error processing page {url.split('page=')[1]}: {e}")
         return None
-    
 
 
 @logger.catch()
@@ -114,7 +123,7 @@ def _looking_for_urls(keywords: str) -> list[str]:
 
 
 @logger.catch()
-async def async_scrapper(*,save_in: str, key_words: str) -> None:
+async def async_scrapper(*, save_in: str, key_words: str) -> None:
     """Create a list of tasks with `goto` coroutine and pass it to `asyncio.wait` coroutine
     then wait for all the results.
     The function takes no arguments and returns None.
@@ -125,48 +134,90 @@ async def async_scrapper(*,save_in: str, key_words: str) -> None:
     # get the list of urls
     pages_urls = _looking_for_urls(keywords=key_words)
     async with async_playwright() as p:
-        logger.info("Connecting to CDP and creating the browser...")
-        browser = await p.chromium.connect_over_cdp(SBR_WS_CDP_LIST)
+        logger.info("Connecting to CDP and creating the browser... ")
+        try:
+            browser = await p.chromium.connect_over_cdp(SBR_WS_CDP_LIST)
+        except playwright._impl._errors.Error as e:  # type: ignore
+            if "Account is suspended" in str(e):
+                print(str(e))
+                rprint(
+                    "[red] Bright data account has been suspended by system. contact me by email: [magenta]onealzero@gmail.com [/magenta] to fix this as soon as possible [/red]"
+                )
+                return
         context_browser = await browser.new_context()
         tasks_list = []
-        logger.info("Creating tasks list...")
-        for idx, url in enumerate(pages_urls,start=0):
-            page = await context_browser.new_page()
-            tasks_list.append( asyncio.create_task(goto_task(url, page),name=f"page_{idx}"))
+        logger.info("Creating tasks list... ")
+        with Progress(console=Console(record=True),) as progress:
+            task = progress.add_task("[green blink] async Scraping...", start=False)
+            s_one = asyncio.Semaphore(value=20)
+            for idx, url in enumerate(pages_urls, start=0):
+                page = await context_browser.new_page()
+                print(idx)
+                async with s_one:
+                    tasks_list.append(
+                        asyncio.create_task(
+                            goto_task(url=url, page=page, task=task, progress=progress, semaphore=s_one)
+                            
+                        )
+                    )
 
-        # run all the tasks and gather the results
-        logger.info("Running all the tasks ...")
-        html_contents = await asyncio.gather(*tasks_list, return_exceptions=False)
-        # html_contents,_ = await asyncio.wait(tasks_list)
-        # append all the results to the global list
-        html_content_list.extend( html_content for html_content in html_contents if isinstance(html_content, str) )
-        print(len(html_content_list))
-        # write the results to a file
+        logger.info("Running all the tasks ... ")
+        html_contents,pending= await asyncio.wait(tasks_list,)
+        # html_contents= await asyncio.gather(*tasks_list,)
+        if isinstance(html_contents,set):
+            html_content_list.extend(
+                (html_content,html_content.cancel())[0]
+                for html_content in html_contents
+                if isinstance(html_content.result(), str)
+            )
+            
+            for i in range(len(pending)):
+                print(pending.pop())
+        elif isinstance(html_contents,list):
+            html_content_list.extend(
+                html_content
+                for html_content in html_contents
+                if isinstance(html_content, str)
+            )  
+        else:
+            pass  
+        # await context_browser.close()
+        # await browser.close()
         write_to_disk(save_in, html_content_list)
+        run_scrapper_with_success(folder_name=save_in)
 
-def sync_scrapper(*,save_in: str, key_words: str):
+
+def sync_scrapper(*, save_in: str, key_words: str):
     playwright = sync_playwright().start()
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()
     pages_urls = _looking_for_urls(keywords=key_words)
-    for idx, url in enumerate(pages_urls,start=0):
-        page = context.new_page()
-        logger.info(f"Loading page {url.split('page=')[1]} ...")
-        response = page.goto(url,wait_until='domcontentloaded',timeout=0)
-        if response is None:
-            return None
-        logger.info(f"Returns the text representation of response body from page {url.split('page=')[1]} ...")
-        html_content = response.text()
-        parsed_content = _browser_parser(html_content=html_content, curr_url=url)
-        html_content_list.append(parsed_content)
-        logger.info(f"Closing the page {url.split('page=')[1]} ...")
-        page.close()
+    with Progress(console=Console(record=True), transient=True) as progress:
+        task = progress.add_task("[green blink] Sync Scraping...", start=False)
+        for idx, url in enumerate(pages_urls, start=0):
+            page = context.new_page()
+            logger.info(f"Loading page {url.split('page=')[1]} ... ")
+            response = page.goto(url, wait_until="domcontentloaded", timeout=0)
+            if response is None:
+                return None
+            logger.info(
+                f"Returns the text representation of response body from page {url.split('page=')[1]} ... "
+            )
+            html_content = response.text()
+            progress.start_task(task)
+            progress.update(task, advance=2.381)
+            parsed_content = _browser_parser(html_content=html_content, curr_url=url)
+            html_content_list.append(parsed_content)
+            logger.info(f"Closing the page {url.split('page=')[1]} ... ")
+            page.close()
     write_to_disk(save_in, html_content_list)
+    run_scrapper_with_success(folder_name=save_in)
+
 
 if __name__ == "__main__":
     start_time = time.perf_counter()
-    asyncio.run(async_scrapper(save_in='pc_lenovo', key_words='pc lenovo'),debug=False)
-    # sync_scrapper(save_in='pc_lenovo', key_words='pc lenovo')
+    # asyncio.run(async_scrapper(save_in='pc_lenovo', key_words='pc lenovo'),debug=False)
+    sync_scrapper(save_in="pc_lenovo", key_words="pc lenovo")
     end_time = time.perf_counter()
     print(f" all those tasks tooks: {end_time-start_time:.2f}")
     ...
