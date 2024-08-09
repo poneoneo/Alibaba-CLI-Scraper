8ab4437d5ddd8acd31724db9ffb78706ba07e0a0
poneoneo <onealzero@gmail.com>
2024-08-09 18:11:05 -0400

    fix(aba_cli_scrapper\__main__):  add pymysql as DBAPI while initializing mysql database without auto-fill flag

--- aba_cli_scrapper/__main__.py
+++ aba_cli_scrapper/__main__.py
@@ -758,7 +758,7 @@ def _db_url(credentials: dict = dict(), auto_fill: bool = False):
                 raise MissingParameter(f"{key} is missing")
         with open("db_credentials.json", "w") as f:
             json.dump(cred, f)
-        return f"mysql+mysqldb://{cred.get('user')}:{cred.get('password')}@{cred.get('host')}/{cred.get('db_name')}"
+        return f"mysql+pymysql://{cred.get('user')}:{cred.get('password')}@{cred.get('host')}/{cred.get('db_name')}"
 
 
 @app.command()
