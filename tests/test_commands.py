1efe769c64cf981ecb3981dfbfbf482f6a1c06a7
poneoneo <onealzero@gmail.com>
2024-09-07 21:13:29 -0400

    ✅ Upgrade code coverage on providers async process

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,28 +1,19 @@
-from faker import Faker
 import pytest
 from aba_cli_scrapper.__main__ import app_t
 from typer.testing import CliRunner
 import socket
+from pathlib import Path
+import shutil
+import subprocess
+import os
 
 
-class block_network(socket.socket):
-	def __init__(self, *args, **kwargs):
-		raise Exception("Network call blocked")
-
-
+html_test_folder = Path("tests/Air_fryers").resolve()
 runner = CliRunner()
+Path("fa.sqlite").unlink()
 
 
-def test_db_init_sqlite_file():
-	"""
-	Test db-init command with sqlite engine and given filename
-	"""
-
-	result = runner.invoke(app_t, ["db-init", "-f", "fa"])
-	assert result.exit_code == 0
-	assert "Database  fa.sqlite  has been created succesfully" in result.stdout
-
-
+####### Test Mysql database######
 def test_db_init_mysql_database():
 	"""
 	Test db-init command with mysql engine and given filename. Since this test schemas has not been created yet, command must inform user to create it.
@@ -32,7 +23,7 @@ def test_db_init_mysql_database():
 		app_t, ["db-init", "mysql", "-u", "Oneal_Dev", "-pw", "0nealDev!te", "-db", "test"]
 	)
 	assert result.exit_code == 0
-	assert "create it first and then try again" in result.stdout
+	assert "It's seems like you want to use mysql engine" in result.stdout
 
 
 def test_db_init_mysql_database_auto_fill():
@@ -45,20 +36,42 @@ def test_db_init_mysql_database_auto_fill():
 	assert "air_fryers  has been created succesfully" in result.stdout
 
 
-def test_db_update_sqlite_file_already_exists():
-	"""
-	Test db-update command with sqlite engine and given filename that already exists.
-	"""
-	result = runner.invoke(app_t, ["db-update", "-f", "fa", "-kr", "./Air_fryers"])
-	assert result.exit_code == 2
-
-
 def test_db_update_mysql_database_already_exists():
 	"""
 	Test db-update command with mysql engine and given db that already exists.
 	"""
-	result = runner.invoke(app_t, ["db-update", "-db", "air_fryers", "-kr", "./Air_fryers"])
+	result = runner.invoke(app_t, ["db-update", "-db", "air_fryers", "-kr", html_test_folder])
 	assert result.exit_code == 2
+
+	####### Test sqlite database######
+
+
+def test_db_init_sqlite_file():
+	"""
+	Test db-init command with sqlite engine and given filename
+	"""
+
+	result = runner.invoke(app_t, ["db-init", "-f", "fa"])
+	assert result.exit_code == 0
+	assert "Database  fa.sqlite  has been created succesfully" in result.stdout
+
+
+def test_db_update_sqlite_file():
+	"""
+	Test db-update command with sqlite engine.
+	"""
+	result = runner.invoke(app_t, ["db-update", "-f", "fa", "-kr", html_test_folder])
+	assert result.exit_code == 0
+
+
+def test_db_update_sqlite_file_already_exists():
+	"""
+	Test db-update command with sqlite engine and given filename that already exists.
+	"""
+	result = runner.invoke(app_t, ["db-update", "-f", "fa", "-kr", html_test_folder])
+	assert result.exit_code == 2
+
+	####### Test export-as-csv ######
 
 
 def test_export_as_csv():
@@ -66,16 +79,14 @@ def test_export_as_csv():
 	assert result.exit_code == 0
 	assert "facsv.csv file has been created with success" in result.stdout
 
+	####### Test ai-agent ######
+
 
 def test_ai_gent():
 	result = runner.invoke(app_t, ["ai-agent", "get all suppliers", "-f", "facsv.csv"])
 	assert result.exit_code == 0
 
-
-def test_scraper_no_internet():
-	socket.socket = block_network
-	result = runner.invoke(app_t, ["scraper", "pc dell", "-pr", "3"])
-	assert result.exit_code == 1
+	####### Test db-update with not found directory ######
 
 
 @pytest.mark.filterwarnings("ignore::pytest.PytestUnraisableExceptionWarning")
@@ -84,10 +95,93 @@ def test_db_update_with_not_found_directory():
 	result = runner.invoke(app_t, ["db-update", "-f", "fa", "-kr", "./Foo_Bar"])
 	assert result.exit_code == 2
 
+	####### Test sync scraper with bright data ######
 
-def test_db_update_sqlite_file_successfully():
-	fake = Faker()
-	sqlite_filename = fake.first_name()[:3]
-	runner.invoke(app_t, ["db-init", "-f", f"{sqlite_filename}"])
-	result = runner.invoke(app_t, ["db-update", "-f", f"{sqlite_filename}", "-kr", "./Air_fryers"])
-	assert f"{sqlite_filename}.sqlite  file has been updated succesfully" in result.stdout
+
+@pytest.mark.filterwarnings("ignore:: RuntimeWarning")
+def test_sync_scraper_with_bright_data():
+	my_env = os.environ.copy()
+	proc = subprocess.Popen(
+		["aba-run", "scraper", "pc dell", "-pr", "2", "-sa"],
+		shell=True,
+		env=my_env,
+		stdout=subprocess.PIPE,
+	)
+	out = proc.stdout
+	assert "Done" in out.read().decode("utf-8")
+	assert Path("pc_dell/").exists() is True
+	assert len(list(Path("pc_dell").glob("*.html"))) == 2
+	shutil.rmtree("pc_dell/")
+
+	####### Test sync scraper with syphoon ######
+
+
+@pytest.mark.filterwarnings("ignore:: RuntimeWarning")
+def test_sync_scraper_with_syphoon():
+	my_env = os.environ.copy()
+	proc = subprocess.Popen(
+		["aba-run", "syphoon-scraper", "pc dell", "-pr", "2", "-sa", "-hf", "pc_dell_custom"],
+		shell=True,
+		env=my_env,
+		stdout=subprocess.PIPE,
+	)
+	out = proc.stdout
+	assert "Done" in out.read().decode("utf-8")
+	assert Path("pc_dell_custom/").exists() is True
+	assert len(list(Path("pc_dell_custom/").glob("*.html"))) == 2
+	shutil.rmtree("pc_dell_custom/")
+
+	####### Test async scraper with bright data ######
+
+
+@pytest.mark.filterwarnings("ignore:: RuntimeWarning")
+def test_async_scraper_with_bright_data():
+	my_env = os.environ.copy()
+	proc = subprocess.Popen(
+		["aba-run", "scraper", "pc dell", "-pr", "2"],
+		shell=True,
+		env=my_env,
+		stdout=subprocess.PIPE,
+	)
+	out = proc.stdout
+	assert "Done" in out.read().decode("utf-8")
+	assert Path("pc_dell/").exists() is True
+	assert len(list(Path("pc_dell/").glob("*.html"))) == 2
+	shutil.rmtree("pc_dell/")
+
+	####### Test async scraper with syphoon ######
+
+
+@pytest.mark.filterwarnings("ignore:: RuntimeWarning")
+def test_async_scraper_with_syphoon():
+	my_env = os.environ.copy()
+	proc = subprocess.Popen(
+		[
+			"aba-run",
+			"syphoon-scraper",
+			"pc dell",
+			"-pr",
+			"2",
+		],
+		shell=True,
+		env=my_env,
+		stdout=subprocess.PIPE,
+	)
+	out = proc.stdout
+	assert "Done" in out.read().decode("utf-8")
+	assert Path("pc_dell/").exists() is True
+	assert len(list(Path("pc_dell/").glob("*.html"))) == 2
+	shutil.rmtree("pc_dell/")
+
+	####### Test scraper with no internet ######
+
+
+class block_network(socket.socket):
+	def __init__(self, *args, **kwargs):
+		raise Exception("Network call blocked")
+
+
+def test_scraper_no_internet():
+	socket.socket = block_network
+	result = runner.invoke(app_t, ["scraper", "pc dell", "-pr", "3"])
+	assert result.exit_code == 1
--- tests/test_db_update_process.py
+++ tests/test_db_update_process.py
@@ -1,12 +1,17 @@
 import pytest
 from aba_cli_scrapper.scrape_from_disk import PageParser
+from pathlib import Path
+
+html_test_folder = Path("tests/Air_fryers").resolve()
+print(html_test_folder)
 
 
 @pytest.mark.filterwarnings("ignore::pytest.PytestUnraisableExceptionWarning")
 @pytest.mark.filterwarnings("ignore:: RuntimeWarning")
 def test_count_suppliers_and_products():
-	page_parser = PageParser(targeted_folder="./Air_fryers")
+	# assert "dfgfg" == html_test_folder
+	page_parser = PageParser(targeted_folder=html_test_folder)
 	suppliers = page_parser.detected_suppliers()
 	products = page_parser.detected_products()
-	assert len(suppliers) == 46
-	assert len(products) == 48
+	assert len(suppliers) == 113
+	assert len(products) == 137
