0424aa7f5ec141f89db8717d0ab654de4be266ef
poneoneo <onealzero@gmail.com>
2024-09-12 20:30:55 -0400

    📝 Add current version number to init file

--- aba_cli_scrapper/__init__.py
+++ aba_cli_scrapper/__init__.py
@@ -675,7 +675,11 @@ Public License instead of this License.  But first, please read
 
 import os
 
+from .ascii import get_current_version
+
 from dotenv import load_dotenv
+
+__version__ = get_current_version()
 
 load_dotenv(override=True)
 BRIGHT_DATA_API_KEY: str = os.environ.get("BRIGHT_DATA_API_KEY", "")
